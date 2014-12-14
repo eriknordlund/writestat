01873666f3703c11c9510890102e540def78384a
Erik Nordlund <erik.nordlund@gmail.com>
2014-12-14 19:36:11 +0100

    Create project

---
+++ lib/chart.dart
@@ -0,0 +1,45 @@
+library chart;
+
+import 'dart:collection';
+import 'dart:js';
+
+class GoalChart {
+  Duration duration;
+  int goal;
+  LinkedHashMap options;
+  List idealProgress;
+  List progress;
+
+  GoalChart(this.duration, this.goal) {
+    idealProgress = [];
+    var goalIncrement = goal / duration.inDays;
+    for (var day=0; day<=duration.inDays; day++) {
+      idealProgress.add(day*goalIncrement);
+    }
+
+    render();
+  }
+
+  render() {
+    var options = new JsObject.jsify({
+      'chart': {
+        'renderTo': 'container',
+        'type': 'line'
+      },
+      'series': [{
+        'name': 'Future',
+        'dashStyle': 'dot',
+        'color': '#4572A7',
+        'data': idealProgress
+      }, {
+        'name': 'Lotta',
+        //'type': 'area',
+        'type': 'column',
+        'color': '#4572A7',
+        'data': progress
+      }]
+    });
+
+    new JsObject(context['Highcharts']['Chart'], [options]);
+  }
+}
---
+++ web/main.dart
@@ -0,0 +1,79 @@
+import 'dart:html';
+
+import 'package:writestat/chart.dart';
+
+class WriteStat {
+  int      goal;
+  DateTime goalStart;
+  DateTime goalEnd;
+  Duration goalDuration;
+  GoalChart chart;
+
+  WriteStat() {
+    querySelector('#createGoal').onClick.listen(createGoal);
+  }
+
+  createGoal(Event e) {
+    var inputGoal      = querySelector('#inputGoal');
+    var inputStartDate = querySelector('#inputStartDate');
+    var inputEndDate   = querySelector('#inputEndDate');
+    if (inputGoal.value.isEmpty) {
+      inputGoal.value = '50000';
+    }
+    goal      = int.parse(inputGoal.value);
+    goalStart = DateTime.parse(inputStartDate.value);
+    goalEnd   = DateTime.parse(inputEndDate.value);
+    // TODO Add 1 day to duration?
+    goalDuration = goalEnd.difference(goalStart);
+
+    for (var i=1; i<=goalDuration.inDays; i++) {
+      createGoalTrackingInputs(i);
+    }
+
+    chart = new GoalChart(goalDuration, goal);
+
+    e.preventDefault();
+  }
+
+  createGoalTrackingInputs(day) {
+    var goalTrackingForm = querySelector('#goalTrackingForm');
+
+    var formGroup = new DivElement();
+    formGroup.classes.add('form-group form-group-sm');
+    goalTrackingForm.children.add(formGroup);
+
+    var label = new LabelElement();
+    //TODO Create 'for' attribute
+    label.text = 'Dag $day';
+    label.classes.add('col-sm-4');
+    formGroup.children.add(label);
+
+    var input = new InputElement(type: 'input');
+    //TODO Create 'for' attribute
+    input.attributes = { 'type': 'number', 'class': 'form-control' };
+
+    var wrapDiv = new DivElement();
+    wrapDiv.classes.add('col-sm-8');
+    wrapDiv.children.add(input);
+    formGroup.children.add(wrapDiv);
+
+    input.onBlur.listen(recalculateGoal);
+  }
+
+  recalculateGoal(Event e) {
+    var values = [];
+    var inputs = querySelectorAll('#goalTrackingForm input');
+    for (var input in inputs) {
+      if (!input.value.isEmpty) {
+        values.add(int.parse(input.value));
+      }
+    }
+    chart.progress = values;
+    // TODO Render should be done implicit
+    chart.render();
+  }
+}
+
+void main() {
+  WriteStat writeStat = new WriteStat();
+}
